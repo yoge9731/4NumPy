abc91316f5e5326da6dd6d59a2e8710c50a4ea27
YOGEESH K P <yogeesh793@gmail.com>
2025-02-07 11:48:43 +0530

    Add files via upload

---
+++ 4.NumPy.py
@@ -0,0 +1,9 @@
+# Create a 3-D array with two 2-D arrays, both containing two arrays with the values 1,2,3 and 4,5,6:
+import numpy as np
+arr = np.array([[[1,2,3],[4,5,6]],[[1,2,3],[4,5,6]]])
+print(arr)
+
+#2
+import numpy as py
+jo = py.array([[[2,3,4],[5,6,7]],[[10,11,12],[13,14,15]]])
+print(jo)
